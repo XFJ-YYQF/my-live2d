f6d5423344ec3dc9400ee3508d5d02a1390d9e40
Minecraft雪峰君 <xunfengjun666@siiway.org>
2026-02-26 20:32:38 +0800

    Add live2d-autoload.js for loading Live2D resources

---
+++ live2d-autoload.js
@@ -0,0 +1,34 @@
+const live2d_path = 'https://s4.zstatic.net/npm/live2d-widgets@1.0.0-rc.7/dist/';
+
+function loadExternalResource(url, type) {
+  return new Promise((resolve, reject) => {
+    let tag;
+    if (type === 'css') {
+      tag = document.createElement('link');
+      tag.rel = 'stylesheet';
+      tag.href = url;
+    } else if (type === 'js') {
+      tag = document.createElement('script');
+      tag.type = 'module';
+      tag.src = url;
+    }
+    if (tag) {
+      tag.onload = () => resolve(url);
+      tag.onerror = () => reject(url);
+      document.head.appendChild(tag);
+    }
+  });
+}
+
+(async () => {
+  await Promise.all([
+    loadExternalResource(live2d_path + 'waifu.css', 'css'),
+    loadExternalResource(live2d_path + 'waifu-tips.js', 'js')
+  ]);
+  initWidget({
+    waifuPath: live2d_path + 'waifu-tips.json',
+    cdnPath: 'https://fastly.jsdelivr.net/gh/XFJ-YYQF/my-live2d@v1.0.6/',
+    cubism2Path: live2d_path + 'live2d.min.js',
+    drag: true,
+  });
+})();
